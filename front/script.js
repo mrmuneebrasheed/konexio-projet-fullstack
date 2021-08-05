ee6304aa61125ddb5e8e8bd550ff284311026ca2
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-05 16:14:19 +0200

    Almost done

--- front/script.js
+++ front/script.js
@@ -1,19 +1,51 @@
 const countryContainer = document.querySelector(".country-list");
 const showDataButton = document.querySelector("#btnShowData");
+const btnReset = document.querySelector("#reset");
+const input = document.querySelector("#name");
+const showAll = document.querySelector("#all");
 console.log;
 let countriesData, html;
 showDataButton.addEventListener("click", function getAllCountries() {
-  const name = document.querySelector("#name").value;
+  const name = input.value;
+  let found = false;
   console.log(name);
   fetch("http://localhost:7000")
     .then((res) => res.json())
     .then((res) => {
       countriesData = res;
       for (let i = 0; i < countriesData.length; i++) {
-        if (countriesData[i].name.toLowerCase() === name.toLowerCase()) {
-          html = ` <li>Country Name: ${countriesData[i].name}, Capital: ${countriesData[i].capital}</li>`;
+        if (
+          countriesData[i].name.toLowerCase() === name.toLowerCase() ||
+          countriesData[i].capital.toLowerCase() === name.toLowerCase() ||
+          countriesData[i].region.toLowerCase() === name.toLowerCase()
+        ) {
+          html = ` <li>Country Name: ${countriesData[i].name}, Capital: ${countriesData[i].capital}, Continent: ${countriesData[i].region} </li>`;
           countryContainer.insertAdjacentHTML("beforeend", html);
+          found = true;
         }
+      }
+      if (found === false) {
+        html = ` <li>This name do not exist</li>`;
+        countryContainer.insertAdjacentHTML("beforeend", html);
+      }
+    });
+});
+
+btnReset.addEventListener("click", function () {
+  countryContainer.innerHTML = "";
+  input.value = "";
+});
+
+showAll.addEventListener("click", function () {
+  fetch("http://localhost:7000")
+    .then((res) => res.json())
+    .then((res) => {
+      countriesData = res;
+      countryContainer.innerHTML = "";
+      for (let i = 0; i < countriesData.length; i++) {
+        html = ` <li>Country Name: ${countriesData[i].name}, Capital: ${countriesData[i].capital}, Continent: ${countriesData[i].region} </li>`;
+        countryContainer.insertAdjacentHTML("beforeend", html);
+        found = true;
       }
     });
 });
