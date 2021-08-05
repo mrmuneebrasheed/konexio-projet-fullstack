1d959fc6a921a37308efd3b37d89a6eb79f406b2
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-05 15:28:26 +0200

    front-back connected

--- front/script.js
+++ front/script.js
@@ -1,6 +1,19 @@
-function getAllCountries() {
+const countryContainer = document.querySelector(".country-list");
+const showDataButton = document.querySelector("#btnShowData");
+console.log;
+let countriesData, html;
+showDataButton.addEventListener("click", function getAllCountries() {
+  const name = document.querySelector("#name").value;
+  console.log(name);
   fetch("http://localhost:7000")
     .then((res) => res.json())
-    .then((res) => console.log(res));
-}
-getAllCountries();
+    .then((res) => {
+      countriesData = res;
+      for (let i = 0; i < countriesData.length; i++) {
+        if (countriesData[i].name.toLowerCase() === name.toLowerCase()) {
+          html = ` <li>Country Name: ${countriesData[i].name}, Capital: ${countriesData[i].capital}</li>`;
+          countryContainer.insertAdjacentHTML("beforeend", html);
+        }
+      }
+    });
+});
